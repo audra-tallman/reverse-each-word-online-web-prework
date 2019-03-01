9e711947dbf9f0dcd495dc8df4e470fb091affa5
Audra Tallman <audra.tallman@hotmail.com>
2019-03-01 23:44:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@
 
 def reverse_each_word(sentence1)
   sentence1 = "Hello there, and how are you?"
-  words = sentence.split
+  words = sentence1.split
   words.each do |word| 
     puts word.reverse
 end
