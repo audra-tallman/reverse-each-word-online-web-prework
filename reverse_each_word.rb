f05734906b0cda96ac54c073d8d53001052979c3
Audra Tallman <audra.tallman@hotmail.com>
2019-03-01 23:52:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,9 +4,15 @@ def reverse_each_word(sentence1)
   sentence1 = "Hello there, and how are you?"
   sentence1.each do |word| 
     puts sentence1.reverse
-end
+  end
 end
 
 def reverse_each_word (sentence2)
   sentence2 = "Hi, again just making sure it's reversed!"
-  sentenc2. 
+  sentenc2.collect do |word|
+    puts sentence2.reverse
+  end
+end
+
+def reverse_each_word (sentence3)
+  sentence ""
