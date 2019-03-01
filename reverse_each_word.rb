f8e39037beb0a39eda99ab44d14b5c9e5227cb57
Audra Tallman <audra.tallman@hotmail.com>
2019-03-01 23:57:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,11 +3,9 @@
 def reverse_each_word(sentence1)
   sentence1 = "Hello there, and how are you?"
   sentence1.split(" ").reverse.join(" ")
-  end
 end
 
 def reverse_each_word (sentence2)
   sentence2 = "Hi, again just making sure it's reversed!"
   sentence2.split(" ").reverse.join(" ")
-  end
 end
