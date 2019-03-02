46932cd7893e820e6121963680d0976dae8a33b1
Audra Tallman <audra.tallman@hotmail.com>
2019-03-02 00:01:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,11 +4,3 @@ def reverse_each_word(sentence)
   sentence.split.collect {|word| word.reverse}.join(" ")
 end  
   
-  sentence1 = "Hello there, and how are you?"
-  sentence1.split().reverse.join(" ")
-end
-
-def reverse_each_word (sentence2)
-  sentence2 = "Hi, again just making sure it's reversed!"
-  sentence2.split(" ").reverse.join(" ")
-end
